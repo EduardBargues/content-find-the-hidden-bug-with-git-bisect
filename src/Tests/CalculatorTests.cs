4852fe9b21fdf4ff2b5f2628cbe048b3d355d689
Eduard <eduardbargues@gmail.com>
2020-12-31 18:56:06 +0100

    test: add tests for multiply functionality

--- src/Tests/CalculatorTests.cs
+++ src/Tests/CalculatorTests.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 using Service;
 
 using Xunit;
@@ -18,6 +20,22 @@ namespace Tests
 
             // ASSERT
             Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Multiply_TwoRandomNumbers_ResultCorrect()
+        {
+            // ARRANGE
+            Calculator calculator = new Calculator();
+            Random rand = new Random();
+            int n1 = rand.Next(1, 10);
+            int n2 = rand.Next(1, 10);
+
+            // ACT
+            int result = calculator.Multiply(n1, n2);
+
+            // ASSERT
+            Assert.Equal(n1 * n2, result);
         }
     }
 }
