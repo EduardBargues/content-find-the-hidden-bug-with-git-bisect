695ff78673216ab3e80db140ef833cb3dc8ee104
Eduard <eduardbargues@gmail.com>
2020-12-31 18:09:53 +0100

    test: add test for sum functionality

---
+++ src/Tests/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿
+using Service;
+
+using Xunit;
+
+namespace Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Sum_SameNumberTwice_ResultShouldBeDoubled()
+        {
+            // ARRANGE
+            Calculator calculator = new Calculator();
+
+            // ACT
+            int result = calculator.Sum(2, 2);
+
+            // ASSERT
+            Assert.Equal(4, result);
+        }
+    }
+}
