cf82ccc3069d8269aeb2bd16b4d131325d5b2507
Eduard <eduardbargues@gmail.com>
2020-12-31 18:56:38 +0100

    docs: add documentation for multiply functionality

--- src/Service/Calculator.cs
+++ src/Service/Calculator.cs
@@ -10,6 +10,12 @@
         /// <returns></returns>
         public int Sum(int number1, int number2) => number1 + number1;
 
+        /// <summary>
+        /// Returns the multiplication of 2 numbers.
+        /// </summary>
+        /// <param name="number1"></param>
+        /// <param name="number2"></param>
+        /// <returns></returns>
         public int Multiply(int number1, int number2) => number1 * number2;
     }
 }
