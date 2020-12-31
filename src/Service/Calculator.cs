4ff8a0154baa71a65545e2aaefbfba9608d80b30
Eduard <eduardbargues@gmail.com>
2020-12-31 18:52:52 +0100

    docs: add documentation for sum functionality (bug introduced here)

--- src/Service/Calculator.cs
+++ src/Service/Calculator.cs
@@ -2,6 +2,12 @@
 {
     public class Calculator
     {
-        public int Sum(int number1, int number2) => number1 + number2;
+        /// <summary>
+        /// Returns the sum of 2 numbers.
+        /// </summary>
+        /// <param name="number1"></param>
+        /// <param name="number2"></param>
+        /// <returns></returns>
+        public int Sum(int number1, int number2) => number1 + number1;
     }
 }
